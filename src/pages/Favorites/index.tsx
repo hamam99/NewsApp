b4923a635c770f1f708134bfa2a7e236ea6be826
BLUEFEATHERZ <zero.saturnus@gmail.com>
2022-10-10 13:22:43 +0700

    create screen favorites

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import {ListNews} from '../../components';
+import {HomeMocks} from '../../mocks/HomeMocks';
 
 const Favorites = () => {
   return (
-    <View>
-      <Text>List Favorites</Text>
+    <View style={styles.container}>
+      <ListNews news={HomeMocks.ListNewsMock.articles} />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 16,
+  },
+});
 export default Favorites;
