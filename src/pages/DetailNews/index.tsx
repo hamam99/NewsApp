7a593fb1356d5548cc0a926792de425c8bea3978
BLUEFEATHERZ <zero.saturnus@gmail.com>
2022-10-10 12:37:30 +0700

    rfactoring

--- src/pages/DetailNews/index.tsx
+++ src/pages/DetailNews/index.tsx
@@ -1,36 +1,70 @@
-import React, {useState, useEffect} from 'react';
-import {Text, useWindowDimensions, View} from 'react-native';
+// import {Button} from '@rneui/base';
+
+import React, {useState} from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  useWindowDimensions,
+  View,
+} from 'react-native';
 import WebView from 'react-native-webview';
-import {HomeMocks} from '../../mocks/HomeMocks';
+import {Icon} from '@rneui/themed';
+
 import {Article} from '../../types/ResponseNews';
 
 const DetailNews = ({navigation, route}) => {
   const news: Article = route.params?.news ?? {};
   console.log('news', news);
 
-  // const widthDevice = useWindowDimensions('')
   const {width} = useWindowDimensions();
-
   const [progress, setProgress] = useState(0);
 
-  return (
-    <View style={{flex: 1}}>
-      {progress < 1 && (
-        <View
-          style={{backgroundColor: 'red', width: progress * width, height: 2}}
-        />
-      )}
+  const handleBackButton = () => {
+    navigation.goBack();
+  };
 
+  return (
+    <View style={styles.container}>
+      {progress < 1 && (
+        <View style={[styles.progressBar, {width: progress * width}]} />
+      )}
       <WebView
         source={{uri: news?.url}}
-        style={{flex: 1}}
+        style={styles.container}
         onLoadProgress={({nativeEvent}) => {
           setProgress(nativeEvent.progress);
           console.log(nativeEvent.progress * 100);
         }}
       />
+      <TouchableOpacity style={styles.backButton} onPress={handleBackButton}>
+        <Icon type="ionicon" name="chevron-back" color="white" />
+      </TouchableOpacity>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  progressBar: {
+    backgroundColor: 'red',
+    height: 2,
+  },
+  backButton: {
+    width: 32,
+    height: 32,
+    backgroundColor: 'red',
+    borderColor: 'red',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    position: 'absolute',
+    top: 8,
+    left: 16,
+    zIndex: 9999,
+  },
+});
 
 export default DetailNews;
