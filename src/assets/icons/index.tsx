61d88d3db62ae7453ac07bc5694e87688c121086
BLUEFEATHERZ <zero.saturnus@gmail.com>
2022-10-10 09:50:41 +0700

    add icon tabs

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,15 @@
+import IconFavoriteSelected from './favorite-selected.png';
+import IconFavoriteUnselected from './favorite-unselected.png';
+import IconHomeSelected from './home-selected.png';
+import IconHomeUnselected from './home-unselected.png';
+import IconProfileSelected from './profile-selected.png';
+import IconProfileUnselected from './profile-unselected.png';
+
+export {
+  IconFavoriteSelected,
+  IconFavoriteUnselected,
+  IconHomeSelected,
+  IconHomeUnselected,
+  IconProfileSelected,
+  IconProfileUnselected,
+};
