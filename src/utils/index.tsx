d28eea6041f8f6c0a9f578c10bb24c3190269ead
Hamam <zero.saturnus@gmail.com>
2022-11-08 22:14:22 +0700

    add index in utils

---
+++ src/utils/index.tsx
@@ -0,0 +1,6 @@
+// import {LocalStorage} from './LocalStorage';
+
+import FavoriteNewsHelper from './FavoriteNewsHelper';
+import LocalStorage from './LocalStorage';
+
+export {LocalStorage, FavoriteNewsHelper};
