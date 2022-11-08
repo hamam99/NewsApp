c6fd12639702d973504bce0f63030be1517370c7
Hamam <zero.saturnus@gmail.com>
2022-11-08 22:13:33 +0700

    load favorites from lcoal storage

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import {useFocusEffect} from '@react-navigation/native';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {ListNews} from '../../components';
-import {HomeMocks} from '../../mocks/HomeMocks';
+import {Article} from '../../types/ResponseNews';
+import {FavoriteNewsHelper} from '../../utils';
 
 const Favorites = () => {
+  const [listFavoriteNews, setFavoriteNews] = useState<Article[]>([]);
+
+  useFocusEffect(
+    React.useCallback(() => {
+      setFavoriteNews(FavoriteNewsHelper.getAll());
+      return () => {};
+    }, []),
+  );
+
   return (
     <View style={styles.container}>
-      <ListNews news={HomeMocks.ListNewsMock.articles} />
+      {/* <ListNews news={HomeMocks.ListNewsMock.articles} /> */}
+      <ListNews news={listFavoriteNews} />
     </View>
   );
 };
