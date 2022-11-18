67b79a374e048df4c3fa76636d4225cc842c0d93
Hamam <zero.saturnus@gmail.com>
2022-11-18 21:56:46 +0700

    wrapping parent component with query client porvider

--- App.tsx
+++ App.tsx
@@ -11,13 +11,16 @@
 import React from 'react';
 import AppNavigation from './src/navigation';
 import Toast from 'react-native-toast-message';
+import {QueryClient, QueryClientProvider} from 'react-query';
+
+const queryClient = new QueryClient();
 
 const App = () => {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppNavigation />
       <Toast />
-    </>
+    </QueryClientProvider>
   );
 };
 
