67a910be72e2cdaa34f8dc9637110e518026ade6
Hamam <zero.saturnus@gmail.com>
2022-11-18 22:23:03 +0700

    implement react query to get news by category

--- src/components/molecules/ListNewsWithCategory/index.tsx
+++ src/components/molecules/ListNewsWithCategory/index.tsx
@@ -1,22 +1,37 @@
 import {View} from 'react-native';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {ListCategoryNews, ListNews} from '../../atoms';
-import {HomeMocks} from '../../../mocks/HomeMocks';
 import {CATEGORY} from '../../../types/Category';
+import {getTopHeadline} from '../../../services';
+import {useQuery} from 'react-query';
 
-type params = {
-  selectedCategory: (a: CATEGORY) => void;
-};
+const ListNewsWithCategory = () => {
+  const [selectedCategory, setSelectedCategory] =
+    useState<CATEGORY>('Business');
 
-const ListNewsWithCategory = ({selectedCategory = () => {}}: params) => {
   const handleSelectCategory = (category: CATEGORY) => {
-    selectedCategory(category);
+    setSelectedCategory(category);
   };
+
+  const callApiTopHeadline = async () => {
+    const {data} = await getTopHeadline(selectedCategory, 20);
+    return data;
+  };
+
+  const {data: {articles: listNews = []} = {}, refetch} = useQuery(
+    'newsByCategory',
+    callApiTopHeadline,
+  );
+
+  useEffect(() => {
+    refetch();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selectedCategory]);
 
   return (
     <View style={{flex: 1}}>
       <ListCategoryNews onPress={category => handleSelectCategory(category)} />
-      <ListNews news={HomeMocks.ListNewsMock.articles} />
+      <ListNews news={listNews} isFavoriteTabSelected={false} />
     </View>
   );
 };
